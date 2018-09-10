780d3b0ceb1495a754874d0ec833cd3642343703
Vlad <emevladstefan95@gmail.com>
2018-09-10 11:39:45 +0300

    changed graph legend behaviour on click

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -25,7 +25,7 @@ export class ChartComponent implements OnInit {
     private timeScale: Array<number> = [];
     private chartData: Array<any> = [];
     private chartConfigs = {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: [],
             datasets: []
@@ -37,13 +37,56 @@ export class ChartComponent implements OnInit {
                 display: true,
                 text: ''
             },
-            legend: {
-                position: 'bottom',
-                labels: {
-                    usePointStyle: true
-                }
-            },
-            scales: {
+			legend: {
+				position: 'bottom',
+				onClick: function(e, legendItem) {
+				  var index = legendItem.datasetIndex;
+				  var ci = this.chart;
+				  var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;       
+				  var anyOthersAlreadyHidden = false;
+				  var allOthersHidden = true;
+		
+				  // figure out the current state of the labels
+				  ci.data.datasets.forEach(function(e, i) {
+					var meta = ci.getDatasetMeta(i);
+					
+					if (i !== index) {
+					  if (meta.hidden) {
+						anyOthersAlreadyHidden = true;
+					  } else {
+						allOthersHidden = false;
+					  }
+					}
+				  });
+				  
+				  // if the label we clicked is already hidden 
+				  // then we now want to unhide (with any others already unhidden)
+				  if (alreadyHidden) {
+					ci.getDatasetMeta(index).hidden = null;
+				  } else { 
+					// otherwise, lets figure out how to toggle visibility based upon the current state
+					ci.data.datasets.forEach(function(e, i) {
+					  var meta = ci.getDatasetMeta(i);
+		
+					  if (i !== index) {
+						// handles logic when we click on visible hidden label and there is currently at least
+						// one other label that is visible and at least one other label already hidden
+						// (we want to keep those already hidden still hidden)
+						if (anyOthersAlreadyHidden && !allOthersHidden) {
+						  meta.hidden = true;
+						} else {
+						  // toggle visibility
+						  meta.hidden = meta.hidden === null ? !meta.hidden : null;
+						}
+					  } else {
+						meta.hidden = null;
+					  }
+					});
+				  }		
+				  ci.update();
+				},
+			  },            
+			  scales: {
                 yAxes: [{
                     ticks: {
                         callback: (label, index, labels) => {
@@ -51,7 +94,8 @@ export class ChartComponent implements OnInit {
                         }
                     },
                 }]
-            },
+			},
+			
             tooltips: {
                 callbacks: {
                     label: (tooltipItem, data) => {
@@ -86,20 +130,20 @@ export class ChartComponent implements OnInit {
 
     initChart() {
     
-        if(this.chartInitialized) {
+        if (this.chartInitialized) {
             this.chart.destroy();
-        }else {
+        } else {
             this.chartInitialized = true;
         }
 
-        this.canvas = document.getElementById("chart2");
-        this.ctx = this.canvas.getContext('2d');
+        this.canvas = this.chartId.nativeElement;
+        
 
         setTimeout(()=>{
             this.canvas.style.height = this.height ? this.height : '400px';
             this.canvas.style.width = this.width ? this.width : null;
 
-            this.chart = new Chart(this.ctx, this.chartConfigs);
+            this.chart = new Chart(this.canvas, this.chartConfigs);
         },500);
 
     }
@@ -115,58 +159,46 @@ export class ChartComponent implements OnInit {
 
         data['lastMonth'].forEach(item => {
             daysLastMonth.push(item._id['day']);
-            salesLastMonthData.push(item.grossSales);
-            profitLastMonthData.push(item.netAmount);
+            salesLastMonthData.push(item.grossSales.toFixed(2));
+            profitLastMonthData.push(item.netAmount.toFixed(2));
         });
 
         this.timeScale = daysLastMonth;
 
         data['monthToDate'].forEach(item => {
-            salesMtdData.push(item.grossSales);
-            profitMtdData.push(item.netAmount);
+            salesMtdData.push(item.grossSales.toFixed(2));
+            profitMtdData.push(item.netAmount.toFixed(2));
         });
 
         const salesMTD = {
             data: salesMtdData,
             dataSources: data.monthToDate,
             label: 'Sales MTD',
-            borderColor: "#3e95cd",
+            backgroundColor: "#3e95cd",
             fill: false,
-            pointStyle: 'circle',
-            pointRadius: 6,
-            pointHoverRadius: 10
         };
         const salesLastMonth = {
             data: salesLastMonthData,
             dataSources: data.lastMonth,
             label: 'Sales Last Month',
-            borderColor: "#8e5ea2",
+            backgroundColor: "#8e5ea2",
             fill: false,
-            pointStyle: 'rectRot',
-            pointRadius: 10,
-            pointHoverRadius: 15
         };
         const profitMTD = {
             data: profitMtdData,
             dataSources: data.monthToDate,
             label: 'Profit MTD',
-            borderColor: "#3cba9f",
+            backgroundColor: "#3cba9f",
             fill: false,
-            pointStyle: 'rect',
-            pointRadius: 8,
-            pointHoverRadius: 12
         };
         const profitLastMonth = {
             data: profitLastMonthData,
             dataSources: data.lastMonth,
             label: 'Profit Last Month',
-            borderColor: "#e8c3b9",
-            fill: false,
-            pointStyle: 'triangle',
-            pointRadius: 8,
-            pointHoverRadius: 12
+            backgroundColor: "#e8c3b9",
+            fill: true,
         };
-
+		
         this.chartData = [salesMTD, salesLastMonth, profitMTD, profitLastMonth];
         this.chartConfigs.options.title.text = 'Month To Date vs Last Month';
         this.chartConfigs.data.labels = this.timeScale;
