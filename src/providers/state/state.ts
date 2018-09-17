5346355e79238421fb5e927185eddb5378ef92e8
Vlad <emevladstefan95@gmail.com>
2018-09-17 15:38:33 +0300

    removed debug message

--- src/providers/state/state.ts
+++ src/providers/state/state.ts
@@ -74,7 +74,7 @@ getHijacks(): Promise<Hijack[]> {
 		
         if (0 !== allPromises.length) {
             Promise.all(allPromises)
-                .then(responses => {console.log( "uita-te aici(state provider)", responses[2]['hijacks'])})
+                .then(responses => {})
                 .catch(err => {
                     this.loading = false;
                     console.log(err);
